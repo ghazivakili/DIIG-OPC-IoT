d1d15c4528d18024665f5beb1b977f4471d5c064
mohammad ghazivakili <m.ghazivakili@gmail.com>
2019-02-18 12:58:16 +0100

    first commit

---
+++ OpcUADIIG-gateway/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/snap/clion/58/bin/cmake/linux/bin/cmake --build /home/photonextdev/workspace/DIIG/OpcUAClient/cmake-build-debug --target all -- -lopen6254
---
+++ OpcUADIIG-gateway/build/CMakeFiles/DIIGatway.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/photonextdev/workspace/DIIG/DIIG-Gateway/src/open62541.c" "/home/photonextdev/workspace/DIIG/DIIG-Gateway/build/CMakeFiles/DIIGatway.dir/src/open62541.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/photonextdev/workspace/DIIG/DIIG-Gateway/src/main-Gateway.cpp" "/home/photonextdev/workspace/DIIG/DIIG-Gateway/build/CMakeFiles/DIIGatway.dir/src/main-Gateway.cpp.o"
+  "/home/photonextdev/workspace/DIIG/DIIG-Gateway/src/snap7.cpp" "/home/photonextdev/workspace/DIIG/DIIG-Gateway/build/CMakeFiles/DIIGatway.dir/src/snap7.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
