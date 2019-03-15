d14731e5b527bfb35efe970cd69310d4032b8d1b
mohammad ghazivakili <m.ghazivakili@gmail.com>
2019-03-15 21:05:26 +0100

    befor change in local to increase the client server

--- OpcUADIIG-IoT/src/standards.h
+++ OpcUADIIG-IoT/src/standards.h
@@ -326,7 +326,7 @@ void * Readdata(void *arguments) {
 
         }
         //pthread_mutex_unlock(&full_mutex);
-        usleep(5000);
+        //usleep(5000);
         nanosleep(&tim, (struct timespec *)NULL);
 
 
